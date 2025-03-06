156f20ad4bfbf94062fbc7aa7dff21edbc1a7308
kermit-frogs19 <dadandboom@gmail.com>
2025-03-06 02:39:50 +0200

    chore: pyproject.toml added

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-from .aiohttp_client import __version__, __name__
-
-setup(
-    name=__name__,
-    version=__version__,
-    packages=find_packages(),
-    install_requires=["aiohttp", "aiolimiter"],
-)
