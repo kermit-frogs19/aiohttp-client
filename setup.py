8adbb0878cb5c7324cfddd1e8206c3e1160a55f1
kermit-frogs19 <dadandboom@gmail.com>
2025-03-06 02:37:55 +0200

    chore: package structure updated

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
-from __init__ import __version__, __name__
+from setuptools import setup, find_packages
+from .aiohttp_client import __version__, __name__
 
 setup(
     name=__name__,
     version=__version__,
-    py_modules=["async_client", "async_client_response", "example"],
+    packages=find_packages(),
     install_requires=["aiohttp", "aiolimiter"],
 )
