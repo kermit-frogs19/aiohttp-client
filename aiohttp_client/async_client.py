31fa8a0af5378f70e784fcf318c64b93be79ca91
kermit-frogs19 <dadandboom@gmail.com>
2025-03-15 19:01:17 +0200

    feat: error retrying logic updated

--- aiohttp_client/async_client.py
+++ aiohttp_client/async_client.py
@@ -115,7 +115,7 @@ class AsyncClient:
         error_name = e.__class__.__name__
         main_message = f"{error_name} {str(e)} in {method} method. kwargs: {kwargs}"
 
-        if not isinstance(e, tuple(self.retryable_errors)):
+        if not isinstance(e.__class__, tuple(self.retryable_errors)):
             return f"Non-retryable error: {main_message}"
         elif attempt_count == self.max_attempts:
             return f"Failed after attempting {attempt_count}/{self.max_attempts} times. {main_message}"
